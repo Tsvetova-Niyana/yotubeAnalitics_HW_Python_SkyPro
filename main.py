8cd2a60430767b24ff52c3f3d1b95b6c16b271e3
Tsvetkova Nina <tsvnishka@gmail.com>
2023-03-21 20:50:48 +0300

    Completing Homework 2 "Access Modes"

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from channel import Channel
 
 if __name__ == '__main__':
-    channel1 = Channel('UCYfYQ1lmPwPdxjBpW_rMJ7w')
+    # Создание экземпляра класса
+    channel1 = Channel("UCYfYQ1lmPwPdxjBpW_rMJ7w")
+
+    # Получение информации о канале
     channel1.print_info()
 
+    # Получение информации о канале из класса(__repr__)
+    print()
+    print(channel1)
+
+    # Получение названия канала
+    print()
+    print(channel1.title_channel)
+
+    # Сохранение сведений о канале в файл json
+    channel1.to_json(f'{channel1.title_channel}.json')
+
+    # Получичение объекта для работы с API вне класса
+    print()
+    print(Channel.get_service())
+
+    # Проверка отсутствия возможности изменения id канала
+    channel1.id = 'efgterhthe'
+    print(channel1.id)
